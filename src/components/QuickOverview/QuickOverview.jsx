278b9492d6d618b8297f1a4656d41bc3dce2e58d
Shaw Kitajima <shawkitajima@gmail.com>
2020-04-03 12:00:08 -0700

    redesigns welcome page

---
+++ src/components/QuickOverview/QuickOverview.jsx
@@ -0,0 +1,60 @@
+import React, {useState, useEffect} from 'react';
+import restaurantService from '../../utils/restaurantService';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import utilities from '../../utils/utilities';
+
+import styles from './QuickOverview.module.css';
+
+const QuickOverview = props => {
+    const [detail, setDetail] = useState({rating: 0});
+    const [loaded, setLoaded] = useState(false);
+    const [imageIdx, setImageIdx] = useState(0);
+
+    useEffect(() => {
+        restaurantService.detail(props.id).then(res => {
+            setDetail(res);
+            setLoaded(true);
+        });
+    }, [props.id])
+    
+    const today = new Date().getDay();
+
+    const incrementImage = () => {
+        if (imageIdx >= detail.photos.length - 1) {
+            setImageIdx(0);
+        }
+        else {
+            setImageIdx(imageIdx + 1);
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            {loaded ? (
+                <div>
+                    <img onClick={() => incrementImage()} className={styles.containerImg} src={detail.photos[imageIdx]} alt="sorry"/>
+                    <div className={styles.details}>
+                        <div>
+                            <h2>{detail.name}</h2>
+                            <div className={styles.ratings}>
+                                <img className={styles.starImg} src={require(`../../pages/RestaurantDetailPage/large_${detail.rating}.png`)} alt="sorry"/>
+                                <span> &nbsp; {detail.review_count} Reviews</span>
+                            </div>
+                            <p>
+                                <span>{detail.price} &nbsp;</span>
+                                {detail.categories.map((category, idx) => (
+                                    <span key={idx}>{category.title} &nbsp;</span>
+                                ))}
+                            </p>
+                            {detail.is_closed ? (<p>Currently Closed</p>) : (<p>Open Until {utilities.formatTime(detail.hours[0].open[today].end)}</p>)}
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <LinearProgress />
+            )}
+        </div>
+    )
+}
+
+export default QuickOverview;
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,25 +1,73 @@
 import React from 'react';
 import styles from './WelcomePage.module.css';
 import { Link } from 'react-router-dom';
-import WelcomeImage from './WelcomeImage.jpg';
+
+import QuickOverview from '../../components/QuickOverview/QuickOverview';
 
 const WelcomePage = props => {
+
+    const restIds = ['J7_y8fSiuqhf9m7oiixZLw', 'KfkPjSTu0OV7yIPTZ4qi3w', '6t0C9lUr3g-hY6oQba4f0w', 'HFwEjoh7L0Lu3Ps7ypkqVg', 'r-59c9YgjqShVvQVI-AGEQ'];
+
     return (
         <div className={styles.container}>
-            <h2>Yelp Dinner Matcher</h2>
-            <img className={styles.imgStyle} src={WelcomeImage} alt="sorry"/>
-            <div className={styles.exp}>
-                <h3>
-                    Do you and your friends ever have trouble finding a place to have dinner? Is Chipotle not the choice? If so, why
-                    don't you both log into our Yelp Dinner Matcher and swipe around on some places open right now? If you add your friends, we'll let
-                    you know if you both liked any places.
-                </h3>
+            <div className={styles.firstSection}>
+                <div className={styles.authSection}>
+                    <div className={styles.authBtn}><Link to='/login'>Log In</Link></div>
+                    <div className={styles.authBtn}><Link to='/signup'>Sign Up</Link></div>
+                </div>
+                <div className={styles.logoSection}>
+                    <h1>yelp matcher</h1>
+                    {/* <div><input type="text" placeholder="Enter Location"/></div>
+                    <div onClick={() => alert('buttons work with divs')}>Explore Food</div> */}
+                </div>
             </div>
-            <div className={styles.row}>
-                <Link to='/login'><button className={styles.welcomePageBtn}>Login</button></Link>
-                <div>or</div>
-                <Link to='/signup'><button className={styles.welcomePageBtn}>Signup</button></Link>
+            <h2>Here Are Some Restaurants Around You!</h2>
+            <div className={styles.secondSection}>
+                {restIds.map((rest, idx) => (
+                    <QuickOverview key={idx} id={rest} />
+                ))}
             </div>
+            <div className={styles.thirdSection}>
+                <div className={styles.feature}>
+                    <img className={styles.featureImg} src={require('./detail.png')} alt=""/>
+                    <h2>Browse Restaurants</h2>
+                    <div className={styles.featureUnderline}></div>
+                    <p>We'll look at your location and suggest restaurants that you may like. You can
+                        choose to like restaurants and even share them with your friends on our app!
+                    </p>
+                </div>
+                <div className={styles.feature}>
+                    <img className={styles.featureImg} src={require('./search.png')} alt=""/>
+                    <h2>Search for Restaurants</h2>
+                    <div className={styles.featureUnderline}></div>
+                    <p>
+                        We don't just randomly suggest restaurants for you - we also allow you to
+                        look for restaurants that you might like!
+                    </p>
+                </div>
+                <div className={styles.feature}>
+                    <img className={styles.featureImg} src={require('./likes.png')} alt=""/>
+                    <h2>View Your Matches</h2>
+                    <div className={styles.featureUnderline}></div>
+                    <p>
+                        If you and your friends both like any restaurants, we'll show you what they are. 
+                        If they don't like any of the same restaurants, are you sure they are your friends?
+                    </p>
+                </div>
+            </div>
+            <div className={styles.fourthSection}>
+                    <h1>
+                        About yelp matcher
+                    </h1>
+                    <p>
+                        We are yelp-matcher. A fun little service that uses the Yelp API, and a lot of our own APIs
+                        to allow users to look and like restaurants in their area. If you have any friends on our
+                        service, you can share restaraunts you see with them. So if you are your friends are ever
+                        struggling to decide where to get dinner, why not look at your matching likes on our 
+                        service?
+                    </p>
+            </div>
+            <footer>2020 yelp-matcher llc all rights reserved</footer>
         </div>
     )
 }
