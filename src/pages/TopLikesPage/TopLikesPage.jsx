c960363b9bd8b40987599246ec37f32a92e0961b
Shaw Kitajima <shawkitajima@gmail.com>
2020-04-04 12:18:15 -0700

    uses restaurantOverview on topLikesPage

--- src/pages/TopLikesPage/TopLikesPage.jsx
+++ src/pages/TopLikesPage/TopLikesPage.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import restaurantService from '../../utils/restaurantService';
-import LikeOverview from '../../components/LikeOverview/LikeOverview';
+import styles from './TopLikesPage.module.css';
+
+import RestaurantOverview from '../../components/ResterauntOverview/RestaurantOverview';
 
 const TopLikesPage = props => {
     const [topLikes, setTopLikes] = useState([]);
@@ -13,9 +15,11 @@ const TopLikesPage = props => {
     return (
         <div>
             <h1>Here are the top liked restaurants across all users</h1>
-            {topLikes.map((rest, idx) => (
-                < LikeOverview id={rest.like} user={props.user} key={idx} />
-            ))}
+            <div className={styles.flexHori}>
+                {topLikes.map((rest, idx) => (
+                    < RestaurantOverview id={rest.like} user={props.user} key={idx} />
+                ))}
+            </div>
         </div>
     )
 }
