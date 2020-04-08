6e508fe852caf32a83a1a12c9f8053e88b1854bf
Shaw Kitajima <shawkitajima@gmail.com>
2020-04-08 14:36:56 -0700

    allows users to customize grid/row viewing on like page

--- src/pages/LikePage/LikePage.jsx
+++ src/pages/LikePage/LikePage.jsx
@@ -1,10 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import userService from '../../utils/userService';
 import RestaurantOverview from '../../components/ResterauntOverview/RestaurantOverview';
+import GridOnIcon from '@material-ui/icons/GridOn';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import styles from './LikePage.module.css'
 
 const LikePage = props => {
     const [likes, setLikes] = useState([]);
+
+    const [view, setView] = useState(1);
 
     const updateLikes = () => {
         userService.getLikes(props.user._id).then(res => setLikes(res));
@@ -15,12 +20,18 @@ const LikePage = props => {
     }, [props.user])
 
     return (
-        <div className={styles.flexHori}>
-            {likes.map((rest, idx) => (
-                <div key={idx}>
-                    < RestaurantOverview id={rest} key={idx} />
-                </div>
-            ))}
+        <div>
+            <div className={styles.viewIcons}>
+                <MenuIcon onClick={() => setView(1)} />
+                <GridOnIcon onClick={() => setView(0)} />
+            </div>
+            <div className={view ? styles.noWrap : styles.wrap}>
+                {likes.map((rest, idx) => (
+                    <div key={idx}>
+                        < RestaurantOverview id={rest} key={idx} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
